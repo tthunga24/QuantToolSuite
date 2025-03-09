50847b17a2ecf58db19c3430bddee5c965bbc3a9
tthunga24 <tanishqthunga@icloud.com>
2025-03-09 11:33:31 -0700

    Implemented GetPrices()

--- ticker.cpp
+++ ticker.cpp
@@ -1,3 +1,38 @@
 #include "ticker.h"
+#include <deque>
 
-Ticker::Ticker() {}
+Ticker::Ticker(QString symbol_) {
+    symbol = symbol_;
+    UpdatePrices();
+    current_price = 0.0;
+}
+
+void Ticker::UpdatePrices() {
+    //TODO: Implement price fetching from API and store in the vector price_history
+    //Store latest price in current_price
+    current_price = 0.0;
+}
+
+const std::vector<double>& Ticker::GetPrices(int interval, int max_entries) {
+    if (captured_intervals.find(interval) != captured_intervals.end()) {
+        return captured_intervals.at(interval);
+    }
+
+    //using a deque for efficiency since we are inserting elements in the front. will convert to vector.
+    std::deque<double> new_interval;
+    int size = price_history.size();
+
+    for (int i = size - 1; i >= 0; i -= interval) {
+        new_interval.push_front(price_history[i]);
+    }
+
+
+    captured_intervals.insert({interval, std::vector<double>(new_interval.begin(), new_interval.end())});
+    return captured_intervals.at(interval);
+}
+
+double Ticker::GetCurrentPrice() {
+    return current_price;
+}
+
+//If the map needs to be erased to be updated, remember to delete the vectors to avoid memory leaks.
--- ticker.h
+++ ticker.h
@@ -5,14 +5,16 @@
 class Ticker
 {
 public:
-   explicit Ticker(QString symbol);
-    std::vector<double> GetPrices(int interval, int max_entries);
-    void FetchPrices();
+    explicit Ticker(QString symbol);
+    const std::vector<double>& GetPrices(int interval, int max_entries);
+    void UpdatePrices();
+    double GetCurrentPrice();
 
 private:
     QString symbol;
     std::vector<double> price_history;
+    std::map<int, std::vector<double>> captured_intervals;
+    double current_price;
 };
 
 #endif // TICKER_H
-
